89725748400db1e0481f00ac94e2d1bc786a60eb
DaiyanNDahy <daiyanndahy@gmail.com>
2023-09-15 22:00:38 +0600

    adding data

---
+++ without_this/src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    
+    private int id,age;
+
+    private String name;
+
+    public Student (int studentId, String studentName,int studentAge){
+
+        id=studentId;
+
+        name=studentName;
+
+        age=studentAge;
+    }
+}
